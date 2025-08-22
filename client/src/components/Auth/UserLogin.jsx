15fa1aab544228d29c2b68cca27ac43befc5cd28
Rajnish kumar <rajnishmaurya250@gmail.com>
2025-08-22 18:18:21 +0530

    remove admin

--- client/src/components/Auth/UserLogin.jsx
+++ client/src/components/Auth/UserLogin.jsx
@@ -259,7 +259,7 @@ export default function UserLogin() {
             Login to Dashboard
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/"
               className="font-medium text-blue-600 hover:text-blue-500"
